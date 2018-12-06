2c64dda754b801cff650224c2cf9a1d27c0dc8d4
zhuhaoceshi <1873667325@qq.com>
2018-12-06 20:33:55 +0800

    111

--- js/redirect.js
+++ js/redirect.js
@@ -1,2 +1,3 @@
-var oldURL="http://pod.qcji8.cn,http://bt.977k53.cn";
-var newURL="http://weed.p0q7ke.cn";
+//var oldURL="http://pod.qcji8.cn,http://bt.977k53.cn";
+//var newURL="http://weed.p0q7ke.cn";
+var newURL="weed.p0q7ke.cn";
