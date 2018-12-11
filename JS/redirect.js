e6836414c889a9e8fb80c52d00339476cbdb6cf4
vipwxqqcom <45687237+vipwxqqcom@users.noreply.github.com>
2018-12-11 13:27:29 +0800

    Update redirect.js

--- JS/redirect.js
+++ JS/redirect.js
@@ -1,4 +1,4 @@
 //var oldURL="http://pod.qcji8.cn,http://bt.977k53.cn";
 //var newURL="http://weed.p0q7ke.cn";
-var newURL="weed.p0q7ke.cn";
+var newURL="woolk.nm9e.cn";
 var heziURL="tzdsl.p0z46b.cn";
